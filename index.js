7ecf360f357c8d05dda2bdcfdd3bab1c341a13c3
Gaurav <gauravmishra2401@gmail.com>
2022-06-08 01:53:43 +0530

    changes1

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let callsbtn = document.getElementById("callsbtn");
 let span = document.getElementById("span");
 let alltools = document.getElementById("alltools");
 let options = document.getElementById("options");
+let mainSection = document.getElementById("main_section");
+let message = document.getElementById("message");
 
 
 // changing sections
@@ -12,18 +14,24 @@ chatsbtn.addEventListener("click", () => {
     callsbtn.classList.remove("active");
     chatsbtn.classList.add("active");
     span.style.left = "0%";
+    mainSection.style.left = "0%";
+    message.innerHTML = `<span class="material-symbols-outlined">chat</span>`;
 })
 statusbtn.addEventListener("click", () => {
     callsbtn.classList.remove("active");
     chatsbtn.classList.remove("active");
     statusbtn.classList.add("active");
     span.style.left = "33%";
+    mainSection.style.left = "-100%";
+    message.innerHTML = `<img src="camera.png" alt="camera">`;
 })
 callsbtn.addEventListener("click", () => {
     statusbtn.classList.remove("active");
     chatsbtn.classList.remove("active");
     callsbtn.classList.add("active");
     span.style.left = "67%";
+    mainSection.style.left = "-200%";
+    message.innerHTML = `<span class="material-symbols-rounded">add_call</span>`;
 })
 
 
